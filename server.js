e3173ec82095e91bf506a0942d91233b40e29ff9
denoronharj <777nando777@gmail.com>
2025-04-26 02:36:34 -0300

    logo certo

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+
 
 // Rota para teste
 app.get("/", (req, res) => {
